29ac9f347df5f7b32ef5adec26d353a704a27296
nicolanzi1 <nicolanzi@gmail.com>
2020-07-13 17:14:42 +0100

    Wrote the rest of the Hash Map and Linked List methods to learn and practice how and LRU cache works using them

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -2,6 +2,8 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
+  attr_reader :max, :prc
+
   def initialize(max, prc)
     @map = HashMap.new
     @store = LinkedList.new
@@ -14,6 +16,13 @@ class LRUCache
   end
 
   def get(key)
+    if map[key]
+      node = map[key]
+      update_node!(node)
+      node.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -22,14 +31,26 @@ class LRUCache
 
   private
 
+  attr_reader :store, :map
+
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    val = self.prc.call(key)
+    new_node = store.append(key, val)
+    map[key] = new_node
+
+    eject! if count > self.max
+    val
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    node.remove
+    map[node.key] = store.append(node.key, node.val)
   end
 
   def eject!
+    rm_node = store.first
+    rm_node.remove
+    map.delete(rm_node.key)
+    nil
   end
 end
